c383d1befd5d05be70a848f5e4c307b1c62ad17a
AnaisCen <anaiscenz@gmail.com>
2023-04-19 17:03:06 +0200

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-// import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Idea from "./pages/Idea";
 import "./App.css";
 import UserProvider from "./contexts/UserContext";
 import Login from "./components/login/Login";
 import Navbar from "./components/navbar/Navbar";
+import Enterprise from "./pages/Enterprise";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <UserProvider>
-          <Login />
+      <UserProvider>
+        <Login />
+        <Enterprise />
+        <Routes>
           <Route path="/" element={<Idea />} />
-        </UserProvider>
-      </Routes>
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
