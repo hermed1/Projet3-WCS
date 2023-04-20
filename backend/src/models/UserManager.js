90e44c454af10b8d4ca23a5fea76b9c41b8f3e03
Mehdi Charef <me-charef@hotmail.fr>
2023-04-20 17:24:23 +0200

    corrected userManager

--- backend/src/models/UserManager.js
+++ backend/src/models/UserManager.js
@@ -41,11 +41,11 @@ class UserManager extends AbstractManager {
     );
   }
 
-  update(company) {
-    return this.database.query(
-      `update ${this.table} set title = ? where id = ?`,
-      [company.title, company.id]
-    );
+  update(id, user) {
+    return this.database.query(`UPDATE ${this.table} SET ? WHERE id = ?`, [
+      user,
+      id,
+    ]);
   }
 
   findByEmail(email) {
