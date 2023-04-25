b197c249abd88949e0006ebae266010e8b91ec27
AnaisCen <anaiscenz@gmail.com>
2023-04-25 21:58:38 +0200

    logout updated

--- frontend/src/components/logout/Logout.jsx
+++ frontend/src/components/logout/Logout.jsx
@@ -1,16 +1,14 @@
-import { useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserContext";
 import useApi from "../../services/useApi";
 
 function Logout() {
-  const navigate = useNavigate();
   const { setUser } = useUser();
   const api = useApi();
 
   const handleLogout = () => {
-    delete api.defaults.headers.authorization;
+    api.defaults.headers.authorization = `Bearer ${""}`;
     setUser(null);
-    navigate("/");
+    window.location.reload(false);
   };
 
   return (
