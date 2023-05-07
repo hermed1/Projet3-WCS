387599343ca522a4b69d6d57c3ade83ccf29f79b
AnaisCen <anaiscenz@gmail.com>
2023-05-07 11:37:28 +0200

    company id added

--- backend/src/models/UserManager.js
+++ backend/src/models/UserManager.js
@@ -78,10 +78,29 @@ class UserManager extends AbstractManager {
     );
   }
 
+  // findByEmail(email) {
+  //   return this.database.query(`SELECT * FROM ${this.table} WHERE email = ?`, [
+  //     email,
+  //   ]);
+  // }
+
   findByEmail(email) {
-    return this.database.query(`SELECT * FROM ${this.table} WHERE email = ?`, [
+    return this.database.query(
+      `SELECT user.id, firstname,
+      lastname,
       email,
-    ]);
+      dateOfBirth,
+      hashedPassword,
+      liked,
+      profilePicture,
+      creationDate,
+      roleId,
+      teamId,
+      companyId
+      FROM user, team 
+      WHERE email = ? and user.teamId = team.id`,
+      [email]
+    );
   }
 
   findAll() {
